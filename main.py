d3027132874e3a51cc68b8cf490ffae22ea8a2e0
Paul-Louis Estadieu <estadieu.pl@gmail.com>
2017-11-12 11:56:11 +0100

    Solve sensor problem

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from spark_bot import *
 
 print (mraa.getVersion())
 x = mraa.Gpio(29)
-x.dir(mraa.DIR_OUT)
+x.dir(mraa.DIR_IN)
 #but = 5
 L = spark_init()
 
 while True:
-    #but = x.read()
+    but = x.read()
     #print(but)
-    x.write(1)
-    time.sleep(0.2)
-    x.write(0)
-    time.sleep(0.2)
-    spark_send(L[0], L[1], "Emergency situation, please go check the patient")
+    #x.write(1)
+    #time.sleep(0.2)
+    #x.write(0)
+    #time.sleep(0.2)
+    if(but == 1):
+		spark_send(L[0], L[1], "Emergency situation, please go check the patient")
